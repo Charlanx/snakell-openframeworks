6c41b97d42940b7a0e5ab717441c6f996cd63562
Charlanx <32102793+Charlanx@users.noreply.github.com>
2018-04-11 04:47:54 -0500

    .h pushed.

--- src/ll.h
+++ src/ll.h
@@ -1,43 +1,68 @@
-#pragma once
+#ifndef LL_H
+#define LL_H
+
 #include <iostream>
 #include <vector>
 
-#include "snakebody.h"
-
 namespace snakelinkedlist {
 
-// TODO templatize the list?...
-// TODO iterators?...
-
-// Specialied linked list class
+// Template linked list class
+template<typename ElementType>
 class LinkedList {
 
+    // Internal node for linked list.
+    struct ListNode {
+        ListNode() : next_(nullptr) {};
+        ListNode(ElementType v) : next_(nullptr), value_(v) {};
+        ListNode *next_;
+        ElementType value_;
+    };
+    ListNode *head_;
+    int size_;
 public:
-    LinkedList();                                         // Default constructor
-    explicit LinkedList(const std::vector<SnakeBodySegment> &values);  // Initilize from vector
+    LinkedList();                                                   // Default constructor
+    explicit LinkedList(const std::vector<ElementType> &values);    // Initilize from vector
 
     // Big 5
-    LinkedList(const LinkedList& source);                 // Copy constructor
-    LinkedList(LinkedList&& source) noexcept;             // Move constructor
-    ~LinkedList();                                        // Destructor
-    LinkedList& operator=(const LinkedList& source);      // Copy assignment operator
-    LinkedList& operator=(LinkedList&& source) noexcept;  // Move assignment operator
+    LinkedList(const LinkedList& source);                                           // Copy constructor
+    LinkedList(LinkedList&& source) noexcept;                                       // Move constructor
+    ~LinkedList();                                                                  // Destructor
+    LinkedList<ElementType>& operator=(const LinkedList<ElementType>& source);      // Copy assignment operator
+    LinkedList<ElementType>& operator=(LinkedList<ElementType>&& source) noexcept;  // Move assignment operator
 
-    void push_front(SnakeBodySegment value);             // Push value on front
-    void push_back(SnakeBodySegment value);              // Push value on back
-    SnakeBodySegment front() const;                      // Access the front value
-    SnakeBodySegment back() const;                       // Access the back valueW
-    void pop_front();                       // remove front element
-    void pop_back();                        // remove back element
-    int size() const;                       // return number of elements
-    std::vector<SnakeBodySegment> GetVector() const;     // return a vector of the values
-    bool empty() const;                     // check if empty
-    void clear();                           // clear the contents
-    void RemoveNth(int n);                  // remove the Nth emement from the front
-    friend std::ostream& operator<<(std::ostream& os, const LinkedList& list);
-    bool operator==(const LinkedList &rhs) const;
+    void push_front(ElementType value);         // Push value on front
+    void push_back(ElementType value);          // Push value on back
+    ElementType front() const;                  // Access the front value
+    ElementType back() const;                   // Access the back valueW
+    void pop_front();                           // remove front element
+    void pop_back();                            // remove back element
+    int size() const;                           // return number of elements
+    std::vector<ElementType> GetVector() const; // return a vector of the values
+    bool empty() const;                         // check if empty
+    void clear();                               // clear the contents
+    void RemoveNth(int n);                      // remove the Nth element from the front 0 indexed
+    bool operator==(const LinkedList<ElementType> &rhs) const;
+
+    // Iterator
+    class Iterator : std::iterator<std::forward_iterator_tag, ElementType> {
+        ListNode *current_;
+        friend LinkedList<ElementType>;
+    public:
+        Iterator() : current_(nullptr) {};
+        Iterator& operator++();
+        ElementType& operator*();
+        bool operator!=(const Iterator& other);
+    };
+
+    Iterator begin();
+    Iterator end();
 };
 
-bool operator!=(const LinkedList& lhs, const LinkedList &rhs);
+template<typename ElementType>
+std::ostream& operator<<(std::ostream& os, const LinkedList<ElementType>& list);
+
+// needed for template instantiation
+#include "ref_ll.cpp"
 
 } // namespace snakelinkedlist
+#endif //LL_H
